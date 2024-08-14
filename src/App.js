abeb91e91f2b101650ed2305bc910f3f06dc40f4
crock444 <ifriauf@inbox.ru>
2024-08-14 21:43:50 +0700

    init

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Header from "./components/Header/Header";
 
 
 
-
 function App() {
     const {onToggleButton, tg} =useTelegram();
 
@@ -17,7 +16,7 @@ function App() {
 
     return (
         <div className="App">
-
+            <Header/>
             <button onClick={onToggleButton}>toggle</button>
         </div>
     );
