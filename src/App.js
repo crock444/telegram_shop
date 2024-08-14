24319dd6c5433e06c502ef951d92d3c3eec00e1b
crock444 <ifriauf@inbox.ru>
2024-08-14 20:24:47 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./components/hooks/useTelegram";
-import Header from "./components/Header/Header";
+
 
 
 
@@ -15,7 +15,7 @@ function App() {
 
     return (
         <div className="App">
-            <Header/>
+
             <button onClick={onToggleButton}>toggle</button>
         </div>
     );
