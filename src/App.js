cd60982ff9202e27f5c93349935ff8ecc10343be
crock444 <ifriauf@inbox.ru>
2024-08-17 22:31:58 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // src/App.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import ProductCard from './components/ProductCard/ProductCard';
+import SplashScreen from './components/SplashScreen/SplashScreen';
 import './App.css';
 
 const products = [
@@ -18,29 +19,41 @@ const products = [
 ];
 
 const App = () => {
+    const [isSplashVisible, setSplashVisible] = useState(true);
+
+    const handleSplashAnimationEnd = () => {
+        setSplashVisible(false);
+    };
+
     return (
         <>
-            <header className="header">
-                <h1 className="header-title">REEDROP</h1>
-                <button className="header-button">Поддержка</button>
-            </header>
-            <div className="app">
-                <div className="product-list">
-                    {products.map((product, index) => (
-                        <ProductCard
-                            key={index}
-                            image={product.image}
-                            title={product.title}
-                            price={product.price}
-                        />
-                    ))}
-                </div>
-            </div>
+            {isSplashVisible && <SplashScreen onAnimationEnd={handleSplashAnimationEnd} />}
+            {!isSplashVisible && (
+                <>
+                    <header className="header">
+                        <h1 className="header-title">REEDROP</h1>
+                        <button className="header-button">Поддержка</button>
+                    </header>
+                    <div className="app">
+                        <div className="product-list">
+                            {products.map((product, index) => (
+                                <ProductCard
+                                    key={index}
+                                    image={product.image}
+                                    title={product.title}
+                                    price={product.price}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </>
+            )}
         </>
     );
 };
 
 export default App;
+
 
 
 
